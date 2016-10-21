f41dbcfbadc11edbba1e226ebefe5740dd481da5
Simon Duclos <sduclos@excilys.com>
2016-10-21 12:23:35 +0200

    First commit

---
+++ src/fr/excilys/Main.java
@@ -0,0 +1,8 @@
+package fr.excilys;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/fr/excilys/domain/StatutFdt.java
@@ -0,0 +1,7 @@
+package fr.excilys.domain;
+
+/**
+ * Created by sduclos on 21/10/16.
+ */
+public enum StatutFdt {
+}
